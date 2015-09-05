245a54c6a11622d863f84654b37608acfb853e01
Hanjie Liu <Hollis@Hanjies-MacBook-Pro.local>
2015-09-05 03:58:03 -0400

    inital commit

---
+++ Day Perfect/Day Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day Perfect
+//
+//  Created by Hanjie Liu on 9/5/15.
+//  Copyright © 2015 Hanjie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
